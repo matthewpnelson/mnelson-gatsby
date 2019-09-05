06e61e9c816635cbe730f361e146ef3b8498edf3
Matt Nelson <matthewpeternelson@gmail.com>
2019-09-04 22:33:11 -0600

    add all files to deploy website

---
+++ gatsby-config.js
@@ -0,0 +1,61 @@
+module.exports = {
+  siteMetadata: {
+    title: `Matthew Nelson - Data Scientist`,
+    name: `Matt Nelson`,
+    siteUrl: `https://novela.narative.co`,
+    description: `Matthew Nelson's Portfolio. Data Scientist, Professional Engineer, Energy, Creative Enthusiast`,
+    hero: {
+      heading: `Helping you innovate, one creative data project at a time.`,
+      maxWidth: 900,
+    },
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/narative`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/narative`,
+      },
+      {
+        name: `instagram`,
+        url: `https://instagram.com/narative.co`,
+      },
+      {
+        name: `linkedin`,
+        url: `https://www.linkedin.com/company/narative/`,
+      },
+      {
+        name: `dribbble`,
+        url: `https://dribbble.com/narativestudio`,
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: "@narative/gatsby-theme-novela",
+      options: {
+        contentPosts: "content/posts",
+        contentAuthors: "content/authors",
+        basePath: "/",
+        authorsPage: true,
+        sources: {
+          local: true,
+          // contentful: true,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Matthew Nelson - Data Scientist`,
+        short_name: `Matt Nelson`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
+        display: `standalone`,
+        icon: `src/assets/favicon.png`,
+      },
+    },
+  ],
+};
